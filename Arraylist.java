bdd321c379e7ba312d848ee9069a43387a6b4c80
Rojalin-panda <rojalinpanda914@gmail.com>
2024-03-21 10:33:19 +0530

    This is my third commit

---
+++ Arraylist.java
@@ -0,0 +1,31 @@
+package Mycollections;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Iterator;
+
+
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		ArrayList<Integer> list_list= new ArrayList<>();
+		ArrayList<Integer> list1= new ArrayList<>();
+	
+	list.add(9);
+	list.add(7);
+	list.add(6);
+	list.add(3);
+	list.add(4);
+//	for printing arraylist
+	for(int i:list) {
+		System.out.println(i);
+	}
+		}
+
+		
+	}
+
+
